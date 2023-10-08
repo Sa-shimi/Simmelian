2e2036f164e5505689303b1b147fa1c979c00cad
Sasha Piccione <91132864+Sa-shimi@users.noreply.github.com>
2023-10-08 17:53:54 +0200

    Update PhD.py

--- PhD.py
+++ PhD.py
@@ -1,15 +1,4 @@
-import random
-import networkx as nx
-import pandas as pd
-from itertools import combinations
-from random import randrange
-import numpy as np
-import matplotlib.pyplot as plt
-from numpy.linalg import eig
-from networkx.drawing.nx_pydot import graphviz_layout
-
 """
-
 This is the first python script intended for the PhD course "Mathematical models for..XXX".
 The script is intended for teaching purposes and has no peculiar function. Each chunk should be running by itself in the
 console.
@@ -26,8 +15,6 @@ import random
 # your script.
 
 # TODO: YOU CAN ALSO CREATE NOTES FOR YOURSELF BY ADDING A "TODO" AFTER THE "#"
-import matplotlib.pyplot as plt
-import networkx as nx
 
 "You can also use quotes - don't recommend as this is not completely ignored"
 
@@ -83,10 +70,11 @@ print(lst[1])
 
 lst = []
 
-# The append function allows us to add an element (the one in brackets) at the end of the list. there are otherXXXXXXere
 lst.append(0)
-
 print(lst)
+
+# The append function allows us to add an element (the one in brackets) at the end of the list. There are tools that can
+# be used to add data (also un chunks) in different points of an already existing index
 
 lst.append(1)
 lst.append(2)
@@ -94,7 +82,7 @@ lst.append(3)
 lst.append(4)
 lst.append(5)
 
-# what if we want to carry out an iterative action for each element of the list?
+# We can utilize a "for _ in _ :" loop to reiterate a specific action for each element present in a list or array or..
 
 for n in lst:
     print(n)
@@ -105,11 +93,37 @@ for n in range(0, 10):
     lst.append(n)
 print(lst)
 
+# If we want to carry out an action n amount of times, we can use this small trick. Creating an ad hoc "list" between 0
+# and n. Please, be careful. range(0, 10) or range(10) is not a list. list(range(10)) is a list
+
 for n in range(10):
     lst.append(n)
 print(lst)
 
-# let's try a little task:
+# Let us introduce the last python element we need for today: if statements. if statements are useful tools that tell
+# out interpreter if a specific condition is met.
+
+lst_of_number = random.sample(range(20), 10)
+odd_numbers = []
+even_numbers = []
+for n in lst_of_number:
+    if n % 2 == 1:
+        odd_numbers.append(n)
+    elif n % 2 == 0:
+        even_numbers.append(n)
+
+# Sometimes we can shorten our code by eliminating conditions using else (if, of course, it's the only possible
+# remaining condition)
+
+odd_numbers = []
+even_numbers = []
+for n in lst_of_number:
+    if n % 2 == 1:
+        odd_numbers.append(n)
+    else:
+        even_numbers.append(n)
+
+# Let's try a little task:
 # let's create 3 lists, the first list wil contain odd numbers from 0 to 10, te second list will contain even numbers
 # from 0 to 10 and the third, of the same length of the first one, will be composed by the sum of the elements of the
 # previous two lists
@@ -137,12 +151,15 @@ for n in lst:
         print(n)
 
 # task for the first day:
-# consider a sorted list containing numbers from 0 to 1000000. what is the fastest way to find the position of
+# consider a sorted list containing random numbers from 0 to 1000000. what is the fastest way to find the position of
 # a randomly generated number?
 # explain reasoning
 
 
+# here is your randomly generated number
 random_number = random.randrange(100000000)
+
+# here is your sorted list
 lst = list(random.sample(range(100000000), 30000000))
 
 # do not worry! creating this object will take quite some time :/
@@ -169,32 +186,41 @@ while not_yet_found:
         not_yet_found = False
         print(f'This is the number you were looking for: {goal_index + int(len(lst_copy) / 2)}')
 
-incredibly_huge_amount_of_data = []
+"""
 
-tiny_object = incredibly_huge_amount_of_data
+Today we will focus on networks. In particular, the scope is to learn how to create, manage and analyze networks 
+characteristics utilizing python. As before, let us now import the necessary packages.
 
-sasha = 3
-students = 2
-able_to_teach = False
-marco = 2
-unhappy = 2
-happy = 1
-banana = []
-
-
-def is_odd(number_to_check):
-    if number_to_check % 2 == 0:
-        return False
-    else:
-        return True
-
+"""
 
 import networkx as nx
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-###here some stuff that you wrote before
+adj = np.array([[0., 1., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 1.],
+                [0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
+                [1., 1., 0., 0., 0., 0., 1., 0., 1., 1., 1., 0., 0., 1., 0., 0., 0., 1., 0., 0., 1.],
+                [1., 1., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 1.],
+                [1., 1., 0., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 1., 0., 0., 0., 1., 1., 1., 1.],
+                [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
+                [0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 1., 0., 0., 0., 1., 0., 0., 1.],
+                [0., 1., 0., 1., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 1.],
+                [1., 1., 0., 0., 0., 0., 1., 0., 0., 1., 1., 1., 0., 1., 0., 0., 0., 1., 0., 0., 1.],
+                [0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0.],
+                [0., 1., 1., 0., 1., 0., 1., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0.],
+                [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
+                [1., 1., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0.],
+                [0., 1., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 1.],
+                [0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 1., 1., 1., 0.],
+                [1., 1., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 0.],
+                [1., 1., 0., 1., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
+                [1., 1., 1., 1., 0., 0., 1., 0., 1., 0., 1., 0., 0., 1., 1., 0., 0., 0., 0., 1., 1.],
+                [1., 1., 0., 0., 1., 0., 1., 0., 0., 0., 1., 0., 0., 1., 0., 0., 0., 1., 0., 1., 0.],
+                [1., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0., 1., 1., 0., 0., 1., 0., 0., 1.],
+                [0., 1., 1., 1., 0., 0., 1., 0., 0., 0., 0., 1., 0., 1., 0., 0., 0., 1., 0., 1., 0.]])
 
-
-G = nx.barabasi_albert_graph(20, 2)
+G = nx.from_numpy_array(adj)
 
 # Let us look at a couple of features of the network we just created
 
@@ -211,18 +237,16 @@ nx.draw(G, with_labels=True)
 print(nx.betweenness_centrality(G))
 print(nx.closeness_centrality(G))
 print(nx.eigenvector_centrality(G))
-print(nx.katz_centrality(G))
 
 # Ok, cool. But now let's say we want to quickly know who's more central than others...
 
 betCent_sorted = dict(sorted(nx.betweenness_centrality(G).items(), key=lambda item: item[1], reverse=True))
 betClos_sorted = dict(sorted(nx.closeness_centrality(G).items(), key=lambda item: item[1], reverse=True))
 betEige_sorted = dict(sorted(nx.eigenvector_centrality(G).items(), key=lambda item: item[1], reverse=True))
-betKatz_sorted = dict(sorted(nx.katz_centrality(G).items(), key=lambda item: item[1], reverse=True))
 print(betCent_sorted)
 print(betClos_sorted)
 print(betEige_sorted)
-print(betKatz_sorted)
+
 
 # Ok, now let's try to visualize the differences between centralities in terms of
 
@@ -249,14 +273,8 @@ nx.spring_layout(G)
 nx.draw(G, with_labels=True, node_color=colors, cmap=plt.colormaps['Blues'])
 plt.show()
 
-colors = []
-for n in G.nodes:
-    colors.append(float(nx.katz_centrality(G)[n]))
-nx.spring_layout(G)
-nx.draw(G, with_labels=True, node_color=colors, cmap=plt.colormaps['Blues'])
-plt.show()
 
-# Wow! 0 appears to be always the most central person. But is it always the case?
+# Wow! 20 appears to be always the most central person. But is it always the case?
 # Let's create an ad-hoc network, called kite network.
 
 # Let us first create an empty network.
@@ -272,31 +290,29 @@ G1.add_edge(1, 4)
 G1.add_edge(2, 4)
 G1.add_edge(2, 5)
 G1.add_edge(2, 7)
-G1.add_edge(3,4)
-G1.add_edge(3,6)
-G1.add_edge(4,6)
-G1.add_edge(4,7)
-G1.add_edge(4,5)
-G1.add_edge(5,7)
-G1.add_edge(6,7)
-G1.add_edge(6,8)
-G1.add_edge(7,8)
-G1.add_edge(8,9)
-G1.add_edge(9,10)
+G1.add_edge(3, 4)
+G1.add_edge(3, 6)
+G1.add_edge(4, 6)
+G1.add_edge(4, 7)
+G1.add_edge(4, 5)
+G1.add_edge(5, 7)
+G1.add_edge(6, 7)
+G1.add_edge(6, 8)
+G1.add_edge(7, 8)
+G1.add_edge(8, 9)
+G1.add_edge(9, 10)
 nx.spring_layout(G1)
 nx.draw(G1, with_labels=True)
 plt.show()
 
-#Let's see who's more central
+# Let's see who's more central
 
-degCent_sorted=dict(sorted(nx.degree_centrality(G1).items(), key=lambda item: item[1],reverse=True))
-print('Most Central node:',list(degCent_sorted)[0])
-print('Least Central node:',list(degCent_sorted)[-1])
-
-
+degCent_sorted = dict(sorted(nx.degree_centrality(G1).items(), key=lambda item: item[1], reverse=True))
+print('Most Central node:', list(degCent_sorted)[0])
+print('Least Central node:', list(degCent_sorted)[-1])
 
 colors = []
-pos=nx.spring_layout(G1)
+pos = nx.spring_layout(G1)
 for n in G1.nodes:
     colors.append(float(nx.closeness_centrality(G1)[n]))
 nx.spring_layout(G1)
@@ -317,15 +333,6 @@ nx.spring_layout(G1)
 nx.draw(G1, with_labels=True, node_color=colors, cmap=plt.colormaps['Blues'], pos=pos)
 plt.show()
 
-colors = []
-for n in G1.nodes:
-    colors.append(float(nx.katz_centrality(G1)[n]))
-nx.spring_layout(G1)
-nx.draw(G1, with_labels=True, node_color=colors, cmap=plt.colormaps['Blues'], pos=pos)
-plt.show()
-
-
-
 
 """
 Task
@@ -335,10 +342,10 @@ Let's now try what we learnt using a brand new dataset.
 
 Pick and download a dataset at the site https://networkrepository.com/network-data.php 
 
-You can pick whatever network you want but consider that networks with more than 500 nodes start to be heavy. Something 
-with 30/200 nodes should be good!
+You can pick whatever network you want but consider that networks with more than 500 nodes start to be heavy for your 
+machine. Something with 30/200 nodes should be good!
 
-The type of files you will meet are matrix files (mtx).
+The type of files you will meet are matrix files (.mtx).
 
 I'm leaving you the script to open a mtx file and to convert it into a numpy array. Is there a function in networkx to 
 convert a matrix or numpy array into a networkx Graph? (yes, try to find it)
@@ -357,19 +364,15 @@ npmatrix=datamatrix.A
 
 
 
-Try to represent graphically the network you find and to write a quick report with possible explanation regarding what 
-are the possible reasons for discrepancies in terms of centrality in your specific network (consider the subject and the
-type of network you're observing). 
+Try to represent graphically the network and describe what you see.
+ 
+Try some measurement we tested together and, if you want, you can also try something more (check the site regarding 
+networkx). Write a quick report with possible explanation regarding what are the possible reasons for discrepancies in 
+terms of centrality in your specific network (consider the subject and the type of network you're observing). 
 
 Include everything in a .py file and send it to sasha.piccione@unive.it
 
-Remember that the .py script must be executable.
-
-
-
-
-
-
+Remember that the .py script must be executable without mistakes or interruption.
 
 
 
